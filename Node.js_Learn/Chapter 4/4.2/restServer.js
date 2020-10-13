87ecfd3fc4048b9090844da92365b6d6752cf343
Seung jun <poodle1465@daum.net>
2020-10-13 22:38:10 +0900

    Node.js_Learn

--- Node.js_Learn/Chapter 4/4.2/restServer.js
+++ Node.js_Learn/Chapter 4/4.2/restServer.js
@@ -33,17 +33,13 @@ http.createServer(async (req, res) => {
           body += data;
         });
         // 요청의 body를 다 받은 후 실행됨
-        // 여
-        // 기
-        // 부
-        // 터
         return req.on('end', () => {
           console.log('POST 본문(Body):', body);
           const { name } = JSON.parse(body);
           const id = Date.now();
           users[id] = name;
-          res.writeHead(201, { 'Content-Type': 'text/plain; charset=utf-8' });
-          res.end('ok');
+          res.writeHead(201);
+          res.end('등록 성공');
         });
       }
     } else if (req.method === 'PUT') {
@@ -56,16 +52,14 @@ http.createServer(async (req, res) => {
         return req.on('end', () => {
           console.log('PUT 본문(Body):', body);
           users[key] = JSON.parse(body).name;
-          res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-          return res.end('ok');
+          return res.end(JSON.stringify(users));
         });
       }
     } else if (req.method === 'DELETE') {
       if (req.url.startsWith('/user/')) {
         const key = req.url.split('/')[2];
         delete users[key];
-        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-        return res.end('ok');
+        return res.end(JSON.stringify(users));
       }
     }
     res.writeHead(404);
@@ -78,4 +72,4 @@ http.createServer(async (req, res) => {
 })
   .listen(8082, () => {
     console.log('8082번 포트에서 서버 대기 중입니다');
-  });Z
+  });
