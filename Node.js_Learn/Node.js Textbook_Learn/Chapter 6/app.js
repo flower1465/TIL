739fcb9c708bbb1fffaefc47b10a8472110f9aac
Seung jun <poodle1465@daum.net>
2020-10-22 23:39:50 +0900

    Node.js_Learn
    express_module_learn

---
+++ Node.js_Learn/Node.js Textbook_Learn/Chapter 6/app.js
@@ -0,0 +1,13 @@
+const e = require('express');
+const express = require('express');
+
+const app = express();
+app.set('port', precess.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.send('hello, Express');
+});
+
+app.listen(app.get('port', () => {
+    console.log(app.get('port', '빈 포트에서 대기 중'));
+}))
