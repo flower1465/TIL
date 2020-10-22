1915041088083cfa155e447873addf8af4911bc6
Seung jun <poodle1465@daum.net>
2020-10-22 21:53:57 +0900

    Homework
    express_post_learn

---
+++ Homework/Homework_1/homework_express_post.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//     res.send("asdfasdf");
+// })
+
+app.post("/", (req, res) => {
+    console.log("REQUESE OK")   ;
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
