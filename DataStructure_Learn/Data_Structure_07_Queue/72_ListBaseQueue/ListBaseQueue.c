7fd8e6c8a0e962b92ce2c68c6a61cfd7bd0ca77a
Seung jun <poodle1465@daum.net>
2020-10-22 21:59:09 +0900

    DataStructure_Learn
    Queue_Learn

---
+++ DataStructure_Learn/Data_Structure_07_Queue/72_ListBaseQueue/ListBaseQueue.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListBaseQueue.h"
+
+void QueueInit(Queue * pq)
+{
+	pq->front = NULL;
+	pq->rear = NULL;
+}
+
+int QIsEmpty(Queue * pq)
+{
+
+}
+
+void Enqueue(Queue * pq, Data data)
+{
+
+}
+
+Data Dequeue(Queue * pq)
+{
+
+}
+
+Data QPeek(Queue * pq)
+{
+
+}
