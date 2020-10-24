8f40473ba4246c009b25bdf26c47cd569981a06b
Seung jun <poodle1465@daum.net>
2020-10-24 23:01:00 +0900

    Node.js_Learn

--- Node.js_Learn/Node.js Textbook_Learn/Chapter 6/6.1/app.js
+++ Node.js_Learn/Node.js Textbook_Learn/Chapter 6/6.1/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('port', precess.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    res.send('hello, Express');
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(app.get('port', () => {
