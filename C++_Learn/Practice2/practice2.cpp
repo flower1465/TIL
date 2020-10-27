12a71e0c37e2d5a32d90216e6870fa3f509e2c50
Seung jun <poodle1465@daum.net>
2020-10-27 23:42:28 +0900

    C++_Learn

---
+++ C++_Learn/Practice2/practice2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "practice2.h"
+
+using namespace std;
+
+int main()
+{
+    CMyDataEX data;
+    data.SetData(20, 30);
+    cout << data.GetX() << endl;
+    cout << data.GetY() << endl;
+
+    return 0;
+}
