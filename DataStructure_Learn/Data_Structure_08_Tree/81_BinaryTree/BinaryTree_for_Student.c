0345bfde9977d68c9fd76615f59dadba85cd4ff0
Seung jun <poodle1465@daum.net>
2020-10-29 23:32:36 +0900

    DataStructure_Learn

---
+++ DataStructure_Learn/Data_Structure_08_Tree/81_BinaryTree/BinaryTree_for_Student.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree.h"
+
+BTreeNode * MakeBTreeNode(void)
+{
+
+}
+
+BTData GetData(BTreeNode * bt)
+{
+
+}
+
+void SetData(BTreeNode * bt, BTData data)
+{
+
+}
+
+BTreeNode * GetLeftSubTree(BTreeNode * bt)
+{
+
+}
+
+BTreeNode * GetRightSubTree(BTreeNode * bt)
+{
+
+}
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub)
+{
+
+}
+
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub)
+{
+
+}
