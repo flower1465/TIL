b7c520ec26a530fed036a85bf10ca403f96c88c6
Seung jun <poodle1465@daum.net>
2020-11-19 22:22:21 +0900

    Homework
    homework_post_learn

---
+++ Homework/Homework_2/routes/posts.js
@@ -0,0 +1,75 @@
+const router = require("express").Router();
+const { Post } = require("../models");
+
+// const jwt = require("jsonwebtoken");
+// const middleware = require("../middleware/token");
+
+router.post("/create", async (req, res) => {
+  const { text } = req.body;
+  try {
+    await Post.create({
+      text,
+    });
+    res.status(200).json({
+      message: "성공",
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(409).json({
+      message: "실패하셧습니다.",
+    });
+  }
+});
+
+router.post("/delete", async (req, res) => {
+  const { text } = req.body;
+  try {
+    await Post.create({
+      text,
+    });
+    res.status(200).json({
+      message: "성공",
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(409).json({
+      message: "실패하셧습니다.",
+    });
+  }
+});
+
+router.post("/update", async (req, res) => {
+  const { text } = req.body;
+  try {
+    await Post.create({
+      text,
+    });
+    res.status(200).json({
+      message: "성공",
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(409).json({
+      message: "실패하셧습니다.",
+    });
+  }
+});
+
+router.post("/read", async (req, res) => {
+  const { text } = req.body;
+  try {
+    await Post.create({
+      text,
+    });
+    res.status(200).json({
+      message: "성공",
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(409).json({
+      message: "실패하셧습니다.",
+    });
+  }
+});
+
+module.exports = router;
