74873b25a6f3d5c5066267303b71092a64a899d5
Seung jun <poodle1465@daum.net>
2020-11-20 19:49:37 +0900

    Homework
    homework_jwt_learn

--- Homework/Homework_2/middleware/token.js
+++ Homework/Homework_2/middleware/token.js
@@ -8,7 +8,7 @@ const tokenMiddleware = async (req, res, next) => {
   try {
     req.decoded = await jwt.verify(token, req.app.get("jwt-secret"));
     next();
-  } catch (e) {
+  } catch (err) {
     res.status(403).json({
       message: "로그인 되어있지 않음",
     });
