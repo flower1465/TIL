354c4eca0c5298269398ca5eff4de2604375d9db
Seung jun <poodle1465@daum.net>
2020-11-21 23:16:27 +0900

    Homework

--- Homework/Homework_2/routes/posts.js
+++ Homework/Homework_2/routes/posts.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const { Post, User } = require("../models");
 
-// const jwt = require("jsonwebtoken");
-// const middleware = require("../middleware/token");
-
 router.post("/create", async (req, res) => {
   const text = req.body;
   try {
