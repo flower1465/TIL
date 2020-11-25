1b337c819a12df154340ddbdca24db60bd58bc7d
Seung jun <poodle1465@daum.net>
2020-11-25 22:08:40 +0900

    Homework
    email_Learn

--- Homework/Homework_2/routes/index.js
+++ Homework/Homework_2/routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const user = require("./user");
 const post = require("./posts");
+const email = require("./email");
 const middleware = require("../middleware/token");
 
 router.use("/user", user);
 router.use("/post", post);
+router.use("/email", email);
 
 module.exports = router;
